e961e82bf561102e1bb60457cd44ad8dffd45623
ankkaya <staryou@126.com>
2022-02-10 17:16:08 +0800

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    title: 'Ankkaya Blog',
+    description: 'Just play fun',
+    themeConfig: {
+        nav: [
+            {
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: '书籍',
+                link: '/'
+            },
+            {
+                text: '关于',
+                link: '/about/'
+            },
+            {
+                text: '联系我',
+                link: '/contact'
+            }
+        ],
+        sidebar: {
+            '/about/': [
+                {
+                    text: '首页',
+                    link: '/about/index'
+                },
+                {
+                    text: '其他',
+                    link: '/about/other'
+                }
+            ]
+        }
+    }
+}
+
+function getBooksSidebar() {
+    return [
+        {
+
+        }
+    ]
+}
