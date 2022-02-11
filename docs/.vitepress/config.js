57e22a959ffbc824ef88350004688d60465b3f2d
ankkaya <staryou@126.com>
2022-02-11 15:52:55 +0800

    添加 蛤蟆先生去看心理医生 文章

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
             },
             {
                 text: '书籍',
-                link: '/'
+                link: '/books/'
             },
             {
                 text: '关于',
