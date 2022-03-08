2914be6b926a02b264d231693ebc103fc5d02864
ankkaya <staryou@126.com>
2022-03-08 16:13:23 +0800

    编辑目录，添加资源列表

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,44 +1,41 @@
 module.exports = {
-    title: 'Ankkaya Blog',
-    description: 'Just play fun',
-    themeConfig: {
-        nav: [
+  title: "Ankkaya Blog",
+  description: "Just play fun",
+  themeConfig: {
+    nav: [
+      {
+        text: "首页",
+        link: "/",
+      },
+      {
+        text: "书籍",
+        link: "/books/",
+      },
+      {
+        text: "其他",
+        link: "/other/",
+      },
+      {
+        text: "联系我",
+        link: "/contact",
+      },
+    ],
+    sidebar: {
+      "/other": [
+        {
+          text: "其他",
+          children: [
             {
-                text: '首页',
-                link: '/'
-            },
-            {
-                text: '书籍',
-                link: '/books/'
-            },
-            {
-                text: '关于',
-                link: '/about/'
-            },
-            {
-                text: '联系我',
-                link: '/contact'
+              text: "资源",
+              link: "/other/",
             }
-        ],
-        sidebar: {
-            '/about/': [
-                {
-                    text: '首页',
-                    link: '/about/index'
-                },
-                {
-                    text: '其他',
-                    link: '/about/other'
-                }
-            ]
-        }
-    }
-}
+          ],
+        },
+      ],
+    },
+  },
+};
 
 function getBooksSidebar() {
-    return [
-        {
-
-        }
-    ]
+  return [{}];
 }
---
+++ docs/other/index.md
@@ -0,0 +1,17 @@
+## CSS
+[uiGrandients - 分享渐变色](https://uigradients.com)
+
+## JavaScript
+[JavaScript 秘密花园 - JavaScript 开发一些常见错误](https://bonsaiden.github.io/JavaScript-Garden/zh/)
+
+## 待整理
+[ICONSVG - 在线自定义 SVG](https://iconsvg.xyz)
+
+[OpenMoji - 免费表情符号库](https://www.openmoji.org)
+
+[Share Icon - 免费矢量素材图库](https://www.shareicon.net)
+
+[tableconvert - 在线表格编辑器](https://tableconvert.com)
+
+[html5up - HTML5 模板](https://html5up.net/)
+
